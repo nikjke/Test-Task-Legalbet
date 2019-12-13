9f314b58bd1e35dd6a064180b6eef700ba69dfd2
nikjke <nikjke@mail.ru>
2019-12-13 23:09:40 +0400

    created layout

---
+++ src/scripts/index.js
@@ -0,0 +1,36 @@
+import '../styles/index.scss';
+
+function calculateBet(howbet, coef) {
+  return howbet * coef;
+}
+
+function calculateWin(result, howBet) {
+  return result - howBet;
+}
+
+
+const calculatorButtonSubmit = document.getElementById('calculatorButtonSubmit');
+const yourPays = document.getElementById('yourPays');
+const calculatorWrapper = document.getElementById('calculatorWrapper');
+
+calculatorButtonSubmit.addEventListener('click', () => {
+
+  const howBetValue = document.getElementById('howBet').value;
+  const coefficientValue = document.getElementById('coefficient').value;
+
+  if (!(howBetValue && coefficientValue)) return;
+
+  const result = calculateBet(howBetValue, coefficientValue);
+  yourPays.value = result;
+  calculatorWrapper.classList.toggle("hidden");
+  calculatorWrapper.classList.toggle("visible");
+
+  const winSum = document.getElementById('winSum');
+  winSum.value = calculateWin(result, howBetValue);
+  winSum.classList.add("greenColor");
+
+  calculatorButtonSubmit.innerHTML = 'Посчитать';
+  calculatorButtonSubmit.classList.toggle("blue");
+  calculatorButtonSubmit.classList.toggle("red");
+});
+
